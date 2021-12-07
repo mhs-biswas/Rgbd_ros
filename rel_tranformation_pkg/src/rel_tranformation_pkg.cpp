5cab639cf37a4c58b31da2dd8bb1fa22e879e4ed
mhs-biswas <shiladitya.biswas@mhsglobal.com>
2021-12-07 16:53:09 -0500

    Relative Transformation pkg building initiated

---
+++ rel_tranformation_pkg/src/rel_tranformation_pkg.cpp
@@ -0,0 +1,32 @@
+#include<eigen3/Eigen/Geometry>
+#include <opencv2/opencv.hpp>
+#include<iostream>
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <pcl-1.10/pcl/visualization/pcl_visualizer.h>
+#include <pcl-1.10/pcl/common/common_headers.h>
+#include <stdexcept>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <vtkRenderWindow.h>
+#include <typeinfo>
+#include <pcl_ros/point_cloud.h>
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+// Eigen::umeyama()
+
+}
